e23d35aeeeaac1556d0dab95a8ae30040a5b87c6
sumit684 <singh.sumit684@gmail.com>
2019-09-18 01:11:15 +0530

    query update

--- application/models/Data.php
+++ application/models/Data.php
@@ -4,7 +4,9 @@ class Data extends CI_Model{
 	public function getData()
 	{	
 		$this->load->database();
-		$q = $this->db->query('SELECT * FROM studentdata WHERE class=10');
+		$q = $this->db->select('*')
+						->where('class',10)
+						->get('studentdata');
 		return $q;
 	}	
 }
